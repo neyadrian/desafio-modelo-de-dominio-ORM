2fd94b0bfabd01007ed9ff28a91bc27997d52367
neyadrian <neyadrian2018@gmail.com>
2026-08-29 15:18:34 -0300

    criando entidade atividade

---
+++ app/src/main/java/com/desafio/app/model/Atividade.java
@@ -0,0 +1,59 @@
+package com.desafio.app.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+    private String descricao;
+    private double preco;
+
+    public Atividade() {
+
+    }
+
+    public Atividade(Integer id, String nome, String descricao, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
