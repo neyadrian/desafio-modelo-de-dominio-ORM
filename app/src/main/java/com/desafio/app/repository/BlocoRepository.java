e3e3c9ff91c73ae27cfac45fd2255ef8cfe1692f
neyadrian <neyadrian2018@gmail.com>
2026-08-30 18:13:33 -0300

    criando repository Bloco

---
+++ app/src/main/java/com/desafio/app/repository/BlocoRepository.java
@@ -0,0 +1,7 @@
+package com.desafio.app.repository;
+
+import com.desafio.app.model.Bloco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlocoRepository extends JpaRepository<Bloco, Integer> {
+}
