e5b7cdea83f19c6d1107c87825bcac1e6abf3f44
neyadrian <neyadrian2018@gmail.com>
2026-08-30 20:59:36 -0300

    atualizando entidade Categoria

--- app/src/main/java/com/desafio/app/model/Categoria.java
+++ app/src/main/java/com/desafio/app/model/Categoria.java
@@ -2,6 +2,9 @@ package com.desafio.app.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
@@ -11,6 +14,9 @@ public class Categoria {
     private Integer id;
 
     private String descricao;
+
+    @OneToMany(mappedBy = "categoria");
+    private List<Atividade> ativiidades = new ArrayList<>();
 
     public Categoria() {
 
@@ -35,5 +41,9 @@ public class Categoria {
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    public List<Atividade> getAtiviidades() {
+        return ativiidades;
     }
 }
