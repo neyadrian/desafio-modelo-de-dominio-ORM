44802586ccf97e850c0d1552c1f4d4da9d6d41a9
neyadrian <neyadrian2018@gmail.com>
2026-08-31 15:34:22 -0300

    configurando banco de dados

--- app/src/main/resources/import.sql
+++ app/src/main/resources/import.sql
@@ -1,31 +1,21 @@
--- Inserindo Categorias
+
 INSERT INTO tb_categoria (descricao) VALUES ('Curso');
--- ID 1
 INSERT INTO tb_categoria (descricao) VALUES ('Oficina');
--- ID 2
 
--- Inserindo Atividades
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00, 1);
--- ID 1
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.00, 2);
--- ID 2
 
--- Inserindo Blocos (Usando o padrão TIMESTAMP com UTC 'Z' para compatibilidade com o tipo Instant)
+
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z', 1);
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z', 2);
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z', 2);
 
--- Inserindo Participantes
-INSERT INTO tb_participante (nome, email) VALUES ('José Silva', 'jose@gmail.com');
--- ID 1
-INSERT INTO tb_participante (nome, email) VALUES ('Tiago Faria', 'tiago@gmail.com');
--- ID 2
-INSERT INTO tb_participante (nome, email) VALUES ('Maria do Rosário', 'maria@gmail.com');
--- ID 3
-INSERT INTO tb_participante (nome, email) VALUES ('Teresa Silva', 'teresa@gmail.com');
--- ID 4
 
--- Inserindo os relacionamentos Muitos-para-Muitos na tabela associativa
+INSERT INTO tb_participante (nome, email) VALUES ('José Silva', 'jose@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES ('Tiago Faria', 'tiago@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES ('Maria do Rosário', 'maria@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES ('Teresa Silva', 'teresa@gmail.com');
+
 INSERT INTO tb_participante_atividade (participante_id, atividade_id) VALUES (1, 1);
 INSERT INTO tb_participante_atividade (participante_id, atividade_id) VALUES (1, 2);
 INSERT INTO tb_participante_atividade (participante_id, atividade_id) VALUES (2, 1);
