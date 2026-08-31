a734a7bf897f21ce1b1fb01b11bed81d8078ede3
neyadrian <neyadrian2018@gmail.com>
2026-08-31 15:24:48 -0300

    atualizando entidade Participante

--- app/src/main/java/com/desafio/app/model/Participante.java
+++ app/src/main/java/com/desafio/app/model/Participante.java
@@ -2,6 +2,9 @@ package com.desafio.app.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_participante")
 public class Participante {
@@ -12,6 +15,12 @@ public class Participante {
 
     private String nome;
     private String email;
+
+    @ManyToMany
+    @JoinTable(name = "tb_participante_atividade",
+            joinColumns = @JoinColumn(name = "participante_id"),
+            inverseJoinColumns = @JoinColumn(name = "atividade_id"))
+    private List<Atividade> atividades = new ArrayList<>();
 
     public Participante() {
 
@@ -45,5 +54,9 @@ public class Participante {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Atividade> getAtividades() {
+        return atividades;
     }
 }
